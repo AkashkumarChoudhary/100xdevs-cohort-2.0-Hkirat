dc0c5f554f0878bfdd4eeb5ed0ea5890bdef025e
AKASH KUMAR <akcmumbai13579@gmail.com>
2025-09-06 12:31:44 +0530

    changes

--- cohort-2/WEEK12/typescript-apis/src/Map.ts
+++
@@ -1,9 +0,0 @@
-// interface User {
-//     name: string;
-//     age: number;
-// }
-
-// const myMap = new Map<string, User>();
-
-// myMap.set('user1', {name: 'John', age: 25});
-// myMap.set('user2', {name: 'Doe', age: 30});
